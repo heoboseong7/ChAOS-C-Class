c191043e56a23a26abf73e455482a8e375ce3fb9
heoboseong7 <boseong844@gmail.com>
2021-03-16 00:32:26 +0900

    Class #1

---
+++ ascii.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 'A';
+    char b = 65;
+
+    printf("a의 값은 : %d \n", a);
+    printf("b의 값은 : %c \n", b);
+
+    return 0;
+}
---
+++ variable2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 100;
+    float pi = 3.14;
+    char c = 'A';
+
+    printf("a의 값은 : %d\n", a);
+    printf("pi의 값은 : %f\n", pi);
+    printf("c의 값은 : %c\n", c);
+
+    return 0;
+}
